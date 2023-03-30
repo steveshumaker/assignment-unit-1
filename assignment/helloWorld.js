f950d5e6eda8e70af04d2aec794bf0fc4ac7c0ef
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-03-29 20:03:14 -0500

    various javascript work

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,1 +1,52 @@
-console.log('Hello world');
+////Random console log stuff
+// let greeting = 'Hello, world!'
+
+// console.log(greeting);
+
+// let hadDinner = false;
+
+// console.log(hadDinner)
+
+// let children = 6;
+// children = children % 4;
+
+// console.log(children)
+
+// console.log('hello' + ' world')
+
+// console.log('Children: ' + children)
+
+////Selecting IDs with JavaScript
+// let footerP = document.querySelector('#footerParagraph');
+// footerP.innerHTML = 'I love JavaScript'
+
+// //Conditionals
+
+// let dogs = 5;
+
+// if (dogs > 9) {
+//     console.log('I have lots of dogs!');
+// }  else {
+//     console.log('I have a small number of dogs.');
+// }
+
+////Using the triple equals to not convert types
+// let ducks = 70;
+
+// if (ducks !== 100) {
+//     console.log('That is not enough ducks')
+// }
+
+//// Using 'and' and 'or'
+// if (ducks > 80 && ducks < 120){
+//     console.log('Perfect # of ducks');
+// } else if (ducks < 80 && ducks > 60) {
+//     console.log('Medium number of ducks')
+// } else {
+//     console.log('Small number of ducks')
+// }
+
+//// Reversing a boolean
+// if (!(ducks > 120)) {
+//     console.log('hello')
+// }
